4b70d72d2a85a13116daa19a2dc8944aff281a94
tmax818 <tylermaxwell661@gmail.com>
2022-03-27 15:47:06 -0700

    update

--- flask_app/controllers/controllers.py
+++ flask_app/controllers/controllers.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, jsonify
 from flask_app import app
 
 @app.route('/')
@@ -9,5 +9,5 @@ def index():
 def github():
     res = {"user_name":"tmax818"}
     
-    return res.json()
+    return jsonify(res)
 
