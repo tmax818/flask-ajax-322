0ad32c70d5a8efccd78930fd5ded08adfac40fd6
tmax818 <tylermaxwell661@gmail.com>
2022-04-27 12:24:59 -0700

    try cors

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
- 
+from flask_cors import CORS, cross_origin
+
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 app.secret_key = "shhhhhh"
--- flask_app/controllers/controllers.py
+++ flask_app/controllers/controllers.py
@@ -1,5 +1,5 @@
 from flask import render_template, jsonify
-from flask_app import app
+from flask_app import app, cross_origin
 
 from flask_app.models.model import Model
 
@@ -14,6 +14,7 @@ def weather():
     return render_template('index.html', weather = Model.weather())
 
 @app.route('/weather/json')
+@cross_origin()
 def weather_json():
     weather = Model.weather()
     return jsonify(weather)
